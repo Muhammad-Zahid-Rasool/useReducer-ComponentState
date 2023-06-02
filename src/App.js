c2ffb66e7bf76b7cc10c65118cda269752a8da87
Zahid <azoocoder@yahoo.comm>
2023-06-02 21:39:23 +0500

    "useReducer is Local Component State Management"

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent1 from './MyComponent1';
+import MyComponent2 from './MyComponent2';
+
+function App() {
+  return (
+    <div className="App">
+    <MyComponent1/>
+    <MyComponent2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyComponent2.js
@@ -0,0 +1,18 @@
+import React, { useReducer } from 'react';
+import { initialState, reducer } from './globalState';
+
+const MyComponent2 = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Access and update state using dispatch function
+
+  return (
+    <div>
+      <h1>Count: {state.count}</h1>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
+    </div>
+  );
+};
+
+export default MyComponent2;
